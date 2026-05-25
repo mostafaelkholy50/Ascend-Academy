d42a06b105c13f2e995b295fb0e7a1489366f22b
Mostafa <mostafaelkholy4321@gmail.com>
2026-05-25 21:30:01 +0300

    feat: schedule automated class and payment reminder commands in console routes

--- routes/console.php
+++ routes/console.php
@@ -9,8 +9,8 @@ use Illuminate\Support\Facades\Schedule;
 // Schedule class reminders to run daily at 8:00 AM
 Schedule::command('class:send-reminders')->dailyAt('08:00');
 
-// Schedule payment reminders to run monthly on the 1st at 9:00 AM
-Schedule::command('payment:send-reminders')->monthlyOn(1, '09:00');
+// Schedule payment reminders to run weekly (every Monday at 9:00 AM)
+Schedule::command('payment:send-reminders')->weeklyOn(1, '09:00');
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
